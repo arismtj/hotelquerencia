4b1cdfff3ae5c18c74b6091cb20c291fe92bbf75
arismtj <70109415+arismtj@users.noreply.github.com>
2025-01-11 09:52:34 -0500

    actualizado 11-01-25
    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,15 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import react from '@astrojs/react';
+
+import node from '@astrojs/node';
+
 export default defineConfig({
-    // ...
-    integrations: [tailwind()],
-   
-  });
+  // ...
+  integrations: [tailwind(), react()],
+
+  adapter: node({
+    mode: 'standalone'
+  })
+});
