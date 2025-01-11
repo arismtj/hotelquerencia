636ba628d0ff24eafa54024c33f72bc401f29a70
arismtj <70109415+arismtj@users.noreply.github.com>
2025-01-11 11:25:36 -0500

    actualizar
    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import amplify from 'astro-aws-amplify';
+
 import tailwind from '@astrojs/tailwind';
 
 import react from '@astrojs/react';
@@ -9,9 +11,10 @@ import node from '@astrojs/node';
 
 export default defineConfig({
   // ...
+  output:'server',
+  
   integrations: [tailwind(), react()],
 
-  adapter: node({
-    mode: 'standalone'
-  })
+  adapter: amplify()
+
 });
